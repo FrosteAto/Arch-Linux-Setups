f13492861f95ffd58fa4dea8dad496766bd85693
FrosteAto <dan.exparrot@protonmail.com>
2026-03-02 22:39:45 +0000

    Changed profiles to make  distinct file names for each version

--- iso-desktop/profiledef.sh
+++ iso-desktop/profiledef.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_name="FrosteArch-desktop"
+iso_label="FROSTEARCH_DESKTOP"
 iso_publisher="Arch Linux <https://archlinux.org>"
 iso_application="Arch Linux Live/Rescue DVD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
