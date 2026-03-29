3cde872c21a87194cc5162c41878d32c9d65f791
FrosteAto <dan.exparrot@protonmail.com>
2026-03-29 15:24:21 +0100

    Add konsave config to dotfile updater

--- payload/manualDotfileUpdate.sh
+++ payload/manualDotfileUpdate.sh
@@ -2,7 +2,7 @@ mkdir -p ~/arch-dotfiles/{config,local/share}
 
 for app in \
   kitty btop nano mpv easyeffects audacity obs-studio \
-  kdenlive krita blender godot gamescope lsp-plugins
+  kdenlive krita blender godot gamescope lsp-plugins konsave
 do
   cp -r ~/.config/$app ~/arch-dotfiles/config/ 2>/dev/null || true
 done
